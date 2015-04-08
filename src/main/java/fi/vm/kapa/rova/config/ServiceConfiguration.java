6ff24ff86338e6a2883ac292646be5b7d53dc036
jkorkalainen <juha.korkalainen@iki.fi>
2015-04-08 08:57:34 +0300

    api key request validation for vtj rest interfaces

--- src/main/java/fi/vm/kapa/rova/config/ServiceConfiguration.java
+++ src/main/java/fi/vm/kapa/rova/config/ServiceConfiguration.java
@@ -1,15 +1,29 @@
 package fi.vm.kapa.rova.config;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import fi.vm.kapa.rova.rest.validation.ValidationContainerRequestFilter;
 import fi.vm.kapa.rova.vtjclient.resources.VTJResource;
 
 @ApplicationPath("/")
 public class ServiceConfiguration extends ResourceConfig {
+	
+	@Value("${api_key}")
+	String apiKey;
+	
 	public ServiceConfiguration() {
 		register(VTJResource.class);
-		//packages("fi.vm.kapa.rova.vtjclient.resources");
+		//register(new ValidationContainerRequestFilter("salainenApiKey"));
+	}
+	
+	@PostConstruct
+	public void init() {
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+		register(new ValidationContainerRequestFilter(apiKey));
 	}
 }
