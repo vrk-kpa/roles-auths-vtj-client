f8d13a73e50ce5b78790b5fd2bc10f53a25d5508
Mikko Tomperi <mikko.tomperi@cybercom.com>
2015-08-13 08:27:01 +0300

    enabled docker build, updated assembly files

--- stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-basepath=$1
-cd $basepath
-export CATALINA_BASE=/data00/services/vtj-client
-export CATALINA_HOME=/devtools/apache-tomcat-8.0.18
-/devtools/apache-tomcat-8.0.18/bin/catalina.sh stop
