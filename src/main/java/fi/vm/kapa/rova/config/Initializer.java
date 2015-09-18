751fa5c237df0a6dd81c5a66bc76b08cb907fb5e
Mikko Tomperi <mikko.tomperi@cybercom.com>
2015-09-18 09:14:06 +0300

    logging property fix

--- src/main/java/fi/vm/kapa/rova/config/Initializer.java
+++ src/main/java/fi/vm/kapa/rova/config/Initializer.java
@@ -1,5 +1,6 @@
 package fi.vm.kapa.rova.config;
 
+import fi.vm.kapa.rova.logging.LogbackConfigurator;
 import fi.vm.kapa.rova.logging.MDCFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -10,12 +11,23 @@ import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.EnumSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class Initializer implements WebApplicationInitializer {
 
     private static final String MDC_FILTER = "mdcFilter";
+
+    @Autowired
+    private LogbackConfigurator logConfigurator;
+
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+        return logConfigurator.containerCustomizer();
+    }
 
     @Override
     public void onStartup(ServletContext ctx) throws ServletException {
