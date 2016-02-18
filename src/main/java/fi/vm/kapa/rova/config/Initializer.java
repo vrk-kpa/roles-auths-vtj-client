634eb34eefafaaf3b462fe026224b97cf39e478e
jteuho <juho.teuho@gofore.com>
2016-02-18 09:27:47 +0200

    KAPAROVA-502 tarvittavat endpointit kaikille projekteille

--- src/main/java/fi/vm/kapa/rova/config/Initializer.java
+++ src/main/java/fi/vm/kapa/rova/config/Initializer.java
@@ -1,7 +1,11 @@
 package fi.vm.kapa.rova.config;
 
+import fi.vm.kapa.rova.engine.evaluation.BaseSpringConfig;
 import fi.vm.kapa.rova.logging.LogbackConfigurator;
 import fi.vm.kapa.rova.logging.MDCFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -11,13 +15,10 @@ import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.EnumSet;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Bean;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class Initializer implements WebApplicationInitializer {
+public class Initializer extends BaseSpringConfig implements WebApplicationInitializer {
 
     private static final String MDC_FILTER = "mdcFilter";
 
