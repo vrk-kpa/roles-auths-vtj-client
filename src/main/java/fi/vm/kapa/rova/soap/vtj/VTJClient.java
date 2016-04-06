7ccdf3b625ab2d86d5211135931dc350c7e5ed5b
jkorkalainen <juha.korkalainen@iki.fi>
2016-04-06 14:15:38 +0300

    clean up

--- src/main/java/fi/vm/kapa/rova/soap/vtj/VTJClient.java
+++ src/main/java/fi/vm/kapa/rova/soap/vtj/VTJClient.java
@@ -3,36 +3,28 @@ package fi.vm.kapa.rova.soap.vtj;
 import fi.vm.kapa.rova.config.SpringPropertyNames;
 import fi.vm.kapa.rova.logging.Logger;
 import fi.vm.kapa.rova.rest.identification.RequestIdentificationFilter;
-import fi.vm.kapa.rova.soap.handlers.XroadHeaderHandler;
 import fi.vm.kapa.rova.soap.vtj.model.VTJResponseMessage;
-import fi.vrk.xml.ws.vtj.vtjkysely._1.*;
+import fi.vrk.xml.ws.vtj.vtjkysely._1.HenkiloTunnusKyselyReqBodyTiedot;
+import fi.vrk.xml.ws.vtj.vtjkysely._1.HenkiloTunnusKyselyResType;
+import fi.vrk.xml.ws.vtj.vtjkysely._1.ISoSoAdapterService60;
+import fi.vrk.xml.ws.vtj.vtjkysely._1.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.Node;
 
-import javax.annotation.PostConstruct;
 import javax.jws.WebService;
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.ws.Holder;
-import javax.xml.ws.handler.Handler;
-import javax.xml.ws.handler.HandlerResolver;
-import javax.xml.ws.handler.PortInfo;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @WebService(endpointInterface = "fi.vrk.xml.rova.vtj.ISoSoAdapterService60")
 public class VTJClient implements SpringPropertyNames {
-
-    SoSoAdapterService60 service = new SoSoAdapterService60();
     ObjectFactory factory = new ObjectFactory();
-
-    @Autowired
-    private XroadHeaderHandler xroadHeaderHandler;
 
     @Autowired
     private HttpServletRequest request;
@@ -49,20 +41,6 @@ public class VTJClient implements SpringPropertyNames {
     private String xrdEndPoint;
 
     private static Logger LOG = Logger.getLogger(VTJClient.class);
-
-    @PostConstruct
-    public void init() {
-        HandlerResolver hs = new HandlerResolver() {
-            @SuppressWarnings("rawtypes")
-            @Override
-            public List<Handler> getHandlerChain(PortInfo portInfo) {
-                List<Handler> handlers = new ArrayList<Handler>();
-                handlers.add(xroadHeaderHandler);
-                return handlers;
-            }
-        };
-        service.setHandlerResolver(hs);
-    }
 
     public VTJResponseMessage getResponse(String hetu, String schema) throws JAXBException {
         LOG.debug("VTJClient.getResponse() starts");
