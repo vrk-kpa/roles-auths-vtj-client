ae2922a2335922e13c948864bd618b35ed299523
Juha Korkalainen <juha.korkalainen@iki.fi>
2017-03-06 13:06:59 +0200

    clean up

--- src/main/java/fi/vm/kapa/rova/vtjclient/resources/VTJResource.java
+++ src/main/java/fi/vm/kapa/rova/vtjclient/resources/VTJResource.java
@@ -33,9 +33,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
-
 @Configuration
 @RestController
 @RequestMapping("/vtj")
