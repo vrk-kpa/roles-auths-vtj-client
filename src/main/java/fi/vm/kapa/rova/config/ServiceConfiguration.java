20ccb1f5796d953628dbd439d21c3180428170b6
Juha Korkalainen <juha.korkalainen@iki.fi>
2017-03-06 13:10:40 +0200

    clean up

--- src/main/java/fi/vm/kapa/rova/config/ServiceConfiguration.java
+++ src/main/java/fi/vm/kapa/rova/config/ServiceConfiguration.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright (c) 2016 Population Register Centre
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -60,10 +60,6 @@ public class ServiceConfiguration {
     @Value("${ssl_truststorepassword}")
     String sslTrustStorePassword;
 
-    public ServiceConfiguration() {
-
-    }
-
     @Bean(name = "apiValidationFilter")
     public FilterRegistrationBean apiValidationFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
@@ -72,7 +68,6 @@ public class ServiceConfiguration {
         filterRegistrationBean.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
         return filterRegistrationBean;
     }
-
 
     @PostConstruct
     public void init() {
